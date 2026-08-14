6d3011a2faa4707b4748bf57abe6db3109c64e0f
Andrew Taylor <134815724+taylorcastle27@users.noreply.github.com>
2026-08-14 16:34:32 +0000

    Add savings interest program and update CMakeLists.txt

---
+++ src/savings_intrest_program.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   double initialSavings;  // User-entered initial savings
+   double interestRate;    // Interest rate
+   double currSavings;     // Current savings with interest
+   int i;                  // Loop variable
+
+   cout << "Enter initial savings: ";
+   cin  >> initialSavings;
+
+   cout << "Enter interest rate: ";
+   cin  >> interestRate;
+
+   cout << endl << "Annual savings for 10 years: " << endl;
+
+   currSavings = initialSavings;
+   for (i = 0; i < 10; ++i) {
+      cout << "$" << currSavings << endl;
+      currSavings = currSavings + (currSavings * interestRate);
+   }
+
+   return 0;
+}
